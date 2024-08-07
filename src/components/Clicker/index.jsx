27fe74c6f021424457d3e6e9563db5f064220429
Dima Prosto <csourgg@gmail.com>
2024-08-07 17:04:47 +0300

    useEffect complite in Clicker

--- src/components/Clicker/index.jsx
+++ src/components/Clicker/index.jsx
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
+import "./Clicker.css";
 
 const Clicker = () => {
-    return (
-        <div>
-            Clicker
-        </div>
-    );
-}
+  const [clickCount, setClickCount] = useState(0);
+
+  useEffect(() => {
+    const handleClick = () => {
+      setClickCount((clickCount) => clickCount + 1);
+    };
+    console.log("add effect");
+    document.body.addEventListener("click", handleClick);
+    return () => {
+      console.log("remove effect");
+      document.body.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  //   document.body.addEventListener('click',handleClick)
+  console.log("render");
+  return (
+    <div>
+      <h2>Clicker: {clickCount}</h2>
+    </div>
+  );
+};
 
 export default Clicker;
+// onClick={handleClick}
