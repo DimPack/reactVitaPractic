4811d02070b9ab332cc9401bbfbb84113e8ef6e5
Dima Prosto <csourgg@gmail.com>
2024-08-07 22:52:33 +0300

    add readi StopWatsh

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -1,28 +1,37 @@
 import { useEffect, useState } from "react";
+import { format, addSeconds } from "date-fns";
 
 const StopWatch = () => {
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(new Date(0, 0, 0, 0, 0, 0, 0));
   const [isRuning, setIsRuning] = useState(false);
+
   const handleIsRunning = () => {
     setIsRuning(!isRuning);
   };
+
+  const handleReset = () => {
+    setTime(new Date(0, 0, 0, 0, 0, 0, 0));
+    setIsRuning(false);
+  };
+
   useEffect(() => {
     if (isRuning) {
-        console.log("add setintrval");
-        const idInterval = setInterval(() => {
-          setTime((time) => time + 1);
-        }, 1000);
-        return () => {
-          clearInterval(idInterval);
-          console.log("clear intrval");
-        };
+      console.log("add setintrval");
+      const idInterval = setInterval(() => {
+        setTime((time) => addSeconds(time, 1));
+      }, 1000);
+      return () => {
+        clearInterval(idInterval);
+        console.log("clear intrval");
+      };
     }
   }, [isRuning]);
 
   return (
     <div>
-      <h2>StopWatch - {time}</h2>
+      <h2>{format(time, "HH : mm : ss")}</h2>
       <button onClick={handleIsRunning}>{isRuning ? "stop" : "start"}</button>
+      <button onClick={handleReset}>reset</button>
     </div>
   );
 };
