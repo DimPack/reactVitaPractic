1326f2b6f1bcb9fe5c42ed0737c484ded0999d54
Dima Prosto <csourgg@gmail.com>
2024-08-07 21:57:08 +0300

    add useEfect StopWatch

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -1,11 +1,30 @@
-
+import { useEffect, useState } from "react";
 
 const StopWatch = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const [time, setTime] = useState(0);
+  const [isRuning, setIsRuning] = useState(false);
+  const handleIsRunning = () => {
+    setIsRuning(!isRuning);
+  };
+  useEffect(() => {
+    if (isRuning) {
+        console.log("add setintrval");
+        const idInterval = setInterval(() => {
+          setTime((time) => time + 1);
+        }, 1000);
+        return () => {
+          clearInterval(idInterval);
+          console.log("clear intrval");
+        };
+    }
+  }, [isRuning]);
+
+  return (
+    <div>
+      <h2>StopWatch - {time}</h2>
+      <button onClick={handleIsRunning}>{isRuning ? "stop" : "start"}</button>
+    </div>
+  );
+};
 
 export default StopWatch;
